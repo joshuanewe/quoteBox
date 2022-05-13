f301ba4d77cd779aa61eee78d5fbe58ddeded994
YabloYescobar <joshuanewe@gmail.com>
2022-05-13 12:37:48 -0700

    all test pass

--- src/Components/quoteBox.js
+++ src/Components/quoteBox.js
@@ -1,4 +1,11 @@
 import React, { useState , useEffect} from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import { CenterFocusStrong } from '@mui/icons-material';
+
 const QuoteBox = () => {
     // eslint-disable-next-line
     const [quotes, setQuotes] = useState(''); 
@@ -16,15 +23,28 @@ const QuoteBox = () => {
     useEffect(() => {
         getQuote();
     }, [])
+    
 
     return ( 
-        <div id='quoteBox'>
-            <h4 id='text'>{quotes.text}</h4>
-            <p id='author'>{quotes.author}</p>
-            <button id='new-quote' onClick={getQuote}>Get quote</button>
-            <button id='tweet-quote'>Tweet</button>
-        </div>
+        <Card id='quote-box' sx={{display: 'inline-block', minWidth: 400, mx: '2px'}}>
+            <h4 id='text' style={{padding: '5px'}}>{quotes.text}</h4>
+            {
+                (quotes.author === null)
+                    ? <p id='author'>Unknown</p>
+                    : <p id='author'>{quotes.author}</p>
+            }
+            <div className='buttonFooter' style={{marginBottom: '5px'}}>
+                <Button variant="outlined" id='new-quote' onClick={getQuote}>Get quote</Button>
+                <Button
+                href={`https://twitter.com/intent/tweet?text=${quotes.text}-${quotes.author}`}
+                target={"_blank"} 
+                variant="outlined" id='tweet-quote'>
+                    Tweet
+                </Button>
+            </div>
+        </Card>
      );
 }
  
 export default QuoteBox;
+// href={`https://twitter.com/intent/tweet?=${}`}
